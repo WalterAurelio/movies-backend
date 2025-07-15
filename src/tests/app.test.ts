9436b105e4315f885d2c793d38eb59fa7fb2c898
WalterAurelio <aureliogareca.dev@gmail.com>
2025-07-15 15:59:32 -0300

    Se configuró el entorno para implementar testing.

---
+++ src/tests/app.test.ts
@@ -0,0 +1,11 @@
+import request from 'supertest';
+
+
+
+describe('POST /register', () => {
+  describe('given a valid firstname, lastname, email and password', () => {
+    // const requestPromise = request()
+
+    test('responds with a 200 status code', async () => {})
+  });
+});
